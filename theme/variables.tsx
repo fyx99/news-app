ee88013b81ff82354bcd1f0363b086112bd026c9
fyx99 <bfelix120@gmail.com>
2022-07-14 14:13:45 +0200

    init

---
+++ theme/variables.tsx
@@ -0,0 +1,38 @@
+/**
+ * Colors
+ */
+export const Colors = {
+
+    transparent: 'rgba(0,0,0,0)',
+    white: '#ffffff',
+    gray: '#888',
+    darkgray: '#555',
+    black: '#000000',
+    background: '#000000',
+    text: '#ffffff',
+    red: '#C91313',
+    green: '#1CA14D',
+    greenOverlay: 'rgba(46, 204, 113, 0.3)',
+    redOverlay: 'rgba(240, 52, 52, 0.3)',
+    whiteOverlay: 'rgba(255,255,255,0.1)'
+  }
+  
+  
+  /**
+   * FontSize
+   */
+export const FontSize = {
+    tiny: 10,
+    small: 14,
+    regular: 20,
+    large: 40,
+}
+
+
+export const Fonts = {
+    RobotoSlab_700Bold
+}
+
+let [fontsLoaded] = useFonts({
+    RobotoSlab_700Bold,
+  });
